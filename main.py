463f9e6d63985dee48c5933d0c0aa15ce4af1b8f
Leonid Yatskevich <53173112+leonidya@users.noreply.github.com>
2022-08-01 10:14:54 +0000

    cipher - last version

--- main.py
+++ main.py
@@ -1,56 +1,43 @@
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def ceaser(text, shift, direction):
-    cipher_text = ""
-    for letter_from_the_word in text:
-        index = alphabet.index(letter_from_the_word)
-        if direction == "encode":
-            index +=shift
-            if index > 26:
-                index = index%26
-        elif direction == "decode":
-            index -=shift
-            if index < 0:
-                index = index+26
-        cipher_text += alphabet[index]
-    print(f'The {direction}d text is {cipher_text}.')
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# def encrypt(text, shift):
-#     cipher_text = ""
-#     for letter_from_the_word in text:
-#         index = alphabet.index(letter_from_the_word)
-#         index +=shift
-#         if index > 26:
-#             index = index%26
-#         cipher_text += alphabet[index]
-#     print(f'The encoded text is {cipher_text}.')
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    #TODO-3: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+    if char not in alphabet:
+        end_text += char
+    else:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
 
-# def decrypt(text, shift):
-#     cipher_text = ""
-#     for letter_from_the_word in text:
-#         index = alphabet.index(letter_from_the_word)
-#         index -=shift
-#         if index < 0:
-#             index = index+26
-#         cipher_text += alphabet[index]
-#     print(f'The encoded text is {cipher_text}.')
+#TODO-1: Import and print the logo from art.py when the program starts.
 
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-ceaser(text, shift, direction)
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+from art import logo
+print(logo)
+restart = ""
+while not restart == "no":    
+    direction = input("Type 'encode' to encrypt, type 'decode' to        decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
+    if shift > 26:
+        shift = shift%26
+    caesar(start_text=text, shift_amount=shift,             
+    cipher_direction=direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.:\n").lower()
