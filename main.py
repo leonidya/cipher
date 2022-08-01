c9252ca07fb80fd21650e89921bc617d8a64009c
Leonid Yatskevich <53173112+leonidya@users.noreply.github.com>
2022-08-01 08:47:15 +0000

    Added the decode method

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift):
     new_encrypted_word = []
@@ -12,8 +8,19 @@ def encrypt(text, shift):
         if index > 26:
             index = index%26
         new_encrypted_word.append(alphabet[index])
-    print(' '.join(new_encrypted_word))
-encrypt(text = text, shift = shift)
+    encrypted_word = ''.join(new_encrypted_word)
+    print(f'The encoded text is {encrypted_word}.')
+
+def decrypt(text, shift):
+    new_encrypted_word = []
+    for letter_from_the_word in text:
+        index = alphabet.index(letter_from_the_word)
+        index -=shift
+        if index < 0:
+            index = index+26
+        new_encrypted_word.append(alphabet[index])
+        encrypted_word = ''.join(new_encrypted_word)
+    print(f'The encoded text is {encrypted_word}.')
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
@@ -28,3 +35,12 @@ encrypt(text = text, shift = shift)
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+if direction == "encode":
+    encrypt(text = text, shift = shift)
+elif direction == "decode":
+    decrypt(text = text, shift = shift)
